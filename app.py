4341754e97276d60ccf2b8cba1e95164a946740e
cwcarrillov <cwcarrillov@outlook.es>
2026-08-10 22:11:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ if modulos == "Módulo Listas":
 elif modulos == "Módulo Arreglos":
   
   st.write("Bienvenido al módulo de Arreglos")
+
+  cantidad_elementos = st.slider("Selecione la cantidad de elementos de su arreglo", 1,100)
+  cantidad_arreglo= np.arange(cantidad_elementos)
+  st.write(cantidad_arreglo)
+
+
   
 else:
   
